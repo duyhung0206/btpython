f9d1018ab07d5eb32e3b949f24004184c4a4cf06
Nguyễn Duy Hùng <duyhung0206@users.noreply.github.com>
2018-07-31 17:20:12 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,83 @@
+var app = angular.module('app', ['ui.bootstrap.materialPicker', 'colorpicker'])
+
+app.directive('shortcut', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: function postLink(scope, iElement, iAttrs) {
+            jQuery(document).on('keypress', function (e) {
+                scope.$apply(scope.keyPressed(e));
+            });
+        }
+    };
+});
+
+
+var fileReader = function ($q, $log) {
+
+    var onLoad = function (reader, deferred, scope) {
+        return function () {
+            scope.$apply(function () {
+                deferred.resolve(reader.result);
+            });
+        };
+    };
+
+    var onError = function (reader, deferred, scope) {
+        return function () {
+            scope.$apply(function () {
+                deferred.reject(reader.result);
+            });
+        };
+    };
+
+    var onProgress = function (reader, scope) {
+        return function (event) {
+            scope.$broadcast("fileProgress",
+                {
+                    total: event.total,
+                    loaded: event.loaded
+                });
+        };
+    };
+
+    var getReader = function (deferred, scope) {
+        var reader = new FileReader();
+        reader.onload = onLoad(reader, deferred, scope);
+        reader.onerror = onError(reader, deferred, scope);
+        reader.onprogress = onProgress(reader, scope);
+        return reader;
+    };
+
+    var readAsDataURL = function (file, scope) {
+        var deferred = $q.defer();
+
+        var reader = getReader(deferred, scope);
+        reader.readAsDataURL(file);
+
+        return deferred.promise;
+    };
+
+    return {
+        readAsDataUrl: readAsDataURL
+    };
+};
+
+app.factory("fileReader",
+    ["$q", "$log", fileReader]);
+
+app.directive("ngFileSelect", function () {
+    return {
+        link: function ($scope, el) {
+            el.bind("change", function (e) {
+                $scope.file = (e.srcElement || e.target).files[0];
+                $scope.getFile();
+            })
+
+        }
+
+    }
+
+
+})
